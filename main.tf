7495caf694c124b7a0eb2d3c37c8432d83e18c46
shrinivas <shinivassb16@gmail.com>
2024-01-19 23:29:26 +0530

    updated

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ resource "aws_security_group" "main" {
 
 resource "aws_docdb_cluster" "main" {
   cluster_identifier      = "${local.prefix}-docdb"
-  engine                  = "docdb"
+  engine                  = var.engine
   engine_version          = var.engine_version
   master_username         = data.aws_ssm_parameter.username.value
   master_password         = data.aws_ssm_parameter.password.value
